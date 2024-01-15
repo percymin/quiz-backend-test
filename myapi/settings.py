2aa858e1dd0b052b34ff7d6d837ed87a3901d90c
percymin <sammymin@naver.com>
2024-01-15 18:13:02 +0900

    ready for depoly

--- myapi/settings.py
+++ myapi/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhitenoiseOptionsMiddleware",
+    "whitenoise.middleware.WhitenoiseMiddleware",
 ]
 
 ROOT_URLCONF = "myapi.urls"
